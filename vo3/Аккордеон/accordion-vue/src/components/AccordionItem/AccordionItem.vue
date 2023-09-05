cb282b39a3ee7e74b285523f42cc94d047a5f196
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2023-09-05 17:20:23 +0300

    new change

---
+++ vo3/Аккордеон/accordion-vue/src/components/AccordionItem/AccordionItem.vue
@@ -0,0 +1,165 @@
+<template>
+    <div
+        class="tree-item"
+        :class="[
+            isSelectedItem && 'tree-item__selected',
+            isChildren && 'tree-item__children'
+        ]"
+        :style="{paddingLeft: levelPadding ? `${levelPadding}px` : null}"
+        @click="itemClick"
+    >
+    <!-- Подписка на одно и то же событие необходима на всех узлах из за странного бага в webkit на linux-->
+    <!-- событие о клике, по какой то причине не всплывает вверх-->
+        <div
+            class="tree-item__content"
+            @click.stop="itemClick"
+        >
+            <div
+                class="tree-item__wrapper"
+                @click.stop="itemClick"
+            >
+                <accordion-item-icon
+                    v-if="item.icon"
+                    :icon="item.icon"
+                    class="tree-item__icon"
+                />
+                <div
+                    class="tree-item__title"
+                    :title="item.title"
+                    @click.stop="itemClick"
+                >{{item.title}}</div>
+            </div>
+            <accordion-item-expander
+                class="tree-item__expander"
+                v-if="isParent"
+                :is-expended="isExpended"
+                @click="expanderClickHandler"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+import AccordionItemExpander from "../AccordionItemExpander/AccordionItemExpander";
+import AccordionItemIcon from "../AccordionItemIcon/AccordionItemIcon";
+
+export default {
+    name: "AccordionItem",
+    components: {AccordionItemIcon, AccordionItemExpander},
+    props: {
+        item: Object,
+        selectedItem: String,
+        expandedItemsId: Array
+    },
+    emits: ['toggleTree', 'itemSelect'],
+    methods: {
+        itemClick() {
+            // Если у элемента action = false, то разварачиваем его
+            if (!this.item.action) {
+                this.expanderClickHandler()
+            }
+            // Происходит при нажатии на элемент
+            this.$emit('itemSelect', this.item.id)
+        },
+        expanderClickHandler() {
+            // Происходит при нажатии на кнопку разворота узла
+            this.$emit('toggleTree', this.item.id)
+        }
+    },
+    computed: {
+        isSelectedItem() {
+            return this.selectedItem === this.item.id
+        },
+        levelPadding() {
+            return (this.item.level - 1) * 32
+        },
+        isChildren() {
+            return this.item.level > 1
+        },
+        isParent() {
+            return !!this.item['parent@']
+        },
+        isExpended() {
+            return !!this.expandedItemsId.find(id => id === this.item.id)
+        }
+    }
+}
+</script>
+
+<style>
+.tree-item {
+    margin-bottom: 2px;
+    cursor: pointer;
+    align-items: center;
+    -webkit-align-items: center;
+    position: relative;
+    border-radius: 8px;
+    user-select: none;
+    -webkit-user-select: none;
+    height: 46px;
+}
+.tree-item:hover {
+    background: #f3f4f8!important;
+}
+.tree-item__children {
+    height: 31px;
+}
+.tree-item__content {
+    display: flex;
+    display: -webkit-flex;
+    align-items: center;
+    -webkit-align-items: center;
+    justify-content: space-between;
+    -webkit-justify-content: space-between;
+    height: 100%;
+}
+.tree-item__wrapper {
+    display: flex;
+    display: -webkit-flex;
+    align-items: center;
+    -webkit-align-items: center;
+    overflow: hidden;
+    padding: 0 0 0 12px;
+    height: 100%;
+}
+.tree-item__title {
+    font-size: 18px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
+.tree-item__children .tree-item__title {
+    font-size: 16px!important;
+}
+.tree-item__children .tree-item__wrapper {
+    padding: 0 12px 0 12px;
+}
+.tree-item__expander {
+    display: none;
+}
+.tree-item:hover .tree-item__expander,
+.tree-item.tree-item__selected .tree-item__expander
+{
+    display: flex;
+    display: -webkit-flex;
+}
+.tree-item__icon {
+    margin: 0 12px 0 0;
+}
+.tree-item.tree-item__selected {
+    background: #f3f4f8!important;
+}
+.tree-item__selected.tree-item:before {
+    content: '';
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    -webkit-transform: translateY(-50%);
+    left: 0;
+    right: 0;
+    bottom: 0;
+    height: 22px;
+    width: 3px;
+    background: #0c94ff;
+}
+</style>
---
+++ vo3/Аккордеон/accordion-vue/src/components/AccordionItemExpander/AccordionItemExpander.vue
@@ -0,0 +1,68 @@
+<template>
+    <div
+        class="expander__wrapper"
+        :class="[isExpended && 'expander__expended']"
+        @click.stop="clickHandler"
+    >
+        <svg
+            width="12"
+            height="7"
+            viewBox="0 0 12 7"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            class="expander"
+        >
+            <g clip-path="url(#clip0_203_1049)">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M0.331055 1.74329L1.66898 0.256706L6.00002 4.15464L10.3311 0.256706L11.669 1.74329L6.00002 6.84536L0.331055 1.74329Z" class="expander_svg"/>
+            </g>
+            <defs>
+                <clipPath id="clip0_203_1049">
+                    <rect width="12" height="7" fill="white"/>
+                </clipPath>
+            </defs>
+        </svg>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AccordionItemExpander",
+    emits: ['click'],
+    props: {
+        isExpended: Boolean
+    },
+    methods: {
+        clickHandler() {
+            this.$emit('click')
+        }
+    }
+}
+</script>
+
+<style>
+.expander {
+    transform: rotate(-90deg);
+    -webkit-transform: rotate(-90deg);
+}
+.expander__wrapper {
+    padding: 0 12px;
+    height: 100%;
+    justify-content: flex-end;
+    -webkit-justify-content: flex-end;
+    align-items: center;
+    -webkit-align-items: center;
+}
+.expander__expended {
+    display: flex!important;
+}
+.expander__expended > svg {
+    transform: rotate(0);
+    -webkit-transform: rotate(0);
+}
+.expander_svg {
+    fill: #8991a9;
+}
+.expander__wrapper:hover .expander_svg {
+    fill: #313e78;
+}
+</style>
