4654d4349c0d35e20005017c54856ceeca574eb1
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2024-10-17 17:20:47 +0300

    new change

---
+++ vo3/Аккордеон/accordion-vue/src/components/AccordionItemIcon/AccordionItemIcon.vue
@@ -0,0 +1,32 @@
+<template>
+    <img
+        :src="calculatedIcon"
+        class="icon"
+    />
+</template>
+
+<script>
+import iconsLib from "./iconsLib";
+
+export default {
+    name: "AccordionItemIcon",
+    props: {
+       icon: {
+           required: true,
+           type: String
+       }
+    },
+    computed: {
+        calculatedIcon() {
+            return iconsLib[this.icon]
+        }
+    }
+}
+</script>
+
+<style>
+.icon {
+    height: 20px;
+    width: 20px;
+}
+</style>
